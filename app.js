079651c199bdd33c6fef05dd17ef740f2a7027e8
Jacob <jakelausier7878@gmail.com>
2023-08-08 16:12:50 -0400

    added more routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,6 +15,8 @@ app.use(
     saveUninitialized: true,
   })
 );
+//route for login and logout
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
